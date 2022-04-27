e559225a2be5e6b38597d503afc46a10b8926b0d
Alexander <san.fargo@gmail.com>
2022-04-27 22:23:56 +0300

    fourth commit

--- js/lobby.js
+++ js/lobby.js
@@ -1,14 +1,52 @@
-// Скрипт для работы всплывающих подсказок
+//Проверка платформы пользователя 
 
-let popoverTriggerList = [].slice.call( document.querySelectorAll('[data-toggle="popover"]'));
-let popoverList = popoverTriggerList.map( function(popoverTrigger) {
-    return new bootstrap.Popover(popoverTrigger);
-});
+const isMobile = /Mobile|webOS|BlackBerry|IEMobile|MeeGo|mini|Fennec|Windows Phone|Android|iP(ad|od|hone)/i.test(navigator.userAgent);
 
-// Копировать в буфер обмена value кнопки "Пригласить друга"
+// Получение кнопки и поля для ссылки
 
 let inviteButton = document.querySelector('.invite-friend__button');
-inviteButton.addEventListener("pointerdown", () => navigator.clipboard.writeText(inviteButton.value));
+let popup = document.querySelector('.popup');
+
+// Алгоритм действия при условии, что 1 - платформа пользователя ПК, 2 - платформа пользователя телефон
+
+if (isMobile == false) {
+    inviteButton.addEventListener("mouseenter", () => {
+        checkValue (inviteButton);
+        popup.classList.toggle('d-none');
+    });
+    inviteButton.addEventListener("mouseleave", () => {
+        checkValue (inviteButton);
+        popup.classList.toggle('d-none');
+    });
+    inviteButton.addEventListener("click", (event) => copyToClipboard());
+} else {
+    inviteButton.addEventListener('click', () => {
+        popup.classList.toggle('d-none');
+        copyToClipboard();
+    });
+}
+
+// Функция проверки значения элемента
+
+function checkValue (element) {
+    switch (element.textContent) {
+        case 'Скопировать ссылку': 
+        element.innerText = 'Пригласить друга';
+        break;
+        case 'Пригласить друга': 
+        element.innerText = 'Скопировать ссылку';
+        break;
+    }
+}
+
+// Функция копирования в буфер обмена 
+
+function copyToClipboard () {
+    let copyInput = document.querySelector('.popup__url');
+    copyInput.focus();
+    copyInput.select();
+    document.execCommand('copy');
+}
 
 // Функция для вызова модального окна
 
@@ -16,3 +54,4 @@ function showModal (modalId = 'result-output') {
     const modal = new bootstrap.Modal(document.querySelector(`#${modalId}`));
     modal.show();
 }
+
